edb0e6da0fa7467708f690a25e50205e46ab5880
yerkeboolean <erkebulan.mukhamedkali@gmail.com>
2020-08-02 11:47:33 +0600

    initial

---
+++ frontend/static/scripts.js
@@ -0,0 +1,66 @@
+const socket = io();
+const videoGrid = document.getElementById('video-grid');
+const myPeer = new Peer(undefined, {
+   host: 'pegii-dudochnik.herokuapp.com',
+   port: '443',
+   secure: true
+});
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+
+const peers = {};
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(myVideo, stream);
+
+    myPeer.on('call', call => {
+        console.log("called");
+        call.answer(stream);
+
+        const video = document.createElement('video');
+        call.on('stream', userVideoStream => {
+            console.log("call streamed");
+           addVideoStream(video, userVideoStream);
+        });
+    });
+
+    socket.on('user-joined', userId => {
+        connectNewUser(userId, stream);
+    });
+});
+
+socket.on('user-disconnected', userId => {
+    console.log(userId);
+    if (peers[userId]) peers[userId].close();
+});
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream;
+    video.addEventListener('loadedmetadata', () => {
+        video.play();
+    });
+    videoGrid.append(video);
+}
+
+function connectNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        console.log("streaming call");
+        addVideoStream(video, userVideoStream)
+    });
+    call.on('close', () => {
+        console.log("closing call");
+       video.remove();
+    });
+
+    peers[userId] = call;
+}
+
+myPeer.on('open', id => {
+    console.log("opened peer " + id);
+    socket.emit('user-joined', ROOM_ID, id);
+});
---
+++ main.py
@@ -0,0 +1,42 @@
+import socketio, uuid
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
+
+sio = socketio.AsyncServer(async_mode='asgi')
+
+
+@app.get("/")
+def root():
+    return RedirectResponse(f"/{uuid.uuid4()}")
+
+
+@app.get("/{room_id}")
+async def room(request: Request, room_id: str):
+    context = {"request": request, "room_id": room_id}
+    return Jinja2Templates(directory="frontend").TemplateResponse(name="index.html", context=context)
+
+
+@sio.event
+async def connect(sid: str, environ):
+    print(f'{sid} connected from {environ["HTTP_REFERER"]}')
+
+
+@sio.on('user-joined')
+async def join_room(sid: str, room_id, user_id):
+    sio.enter_room(sid, room_id)
+    print(sid + " joined room " + room_id)
+    await sio.emit('user-joined', data=user_id, room=room_id, skip_sid=sid)
+
+
+@sio.event
+async def disconnect(sid: str, *args):
+    print(sid + " disconnected")
+    print(args)
+    await sio.emit('user-disconnected', data=sid, skip_sid=sid)
+
+app.mount("/", socketio.ASGIApp(sio))
